2b0b9e6576f2359aaf01c9757df295c48d1da327
madishak <33802709+madishak@users.noreply.github.com>
2019-08-27 13:26:11 +0300

    Added main.py

--- main.py
+++ main.py
@@ -2,48 +2,59 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from plyer import notification
+from sqlalchemy import select
+# from flask import models
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////chat.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////chat.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'chat.db')
+# app.coSQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 db = SQLAlchemy(app)
 
 
 class Login(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer,  primary_key=True)
     name = db.Column(db.String(1024), nullable=False)
     password = db.Column(db.String(1024), nullable=False)
+    messages = db.relationship('Message', backref='user', lazy='dynamic')
 
-    def __init__(self, name, password):
-        self.name = name.strip()
-        self.password = password.strip()
-        # self.text = [
-        #     Message(text=t.strip()) for t in text.split(',')
-        # ]
+    # def __init__(self, name, password):
+    #     self.name = name.strip()
+    #     self.password = password.strip()
+    #     # self.text = [
+    #     #     Message(text=t.strip()) for t in text.split(',')
+    #     # ]
 
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(32), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('login.id'))
 
-    # message_id = db.Column(db.Integer, db.ForeignKey('login.id'), nullable=False)
-    # message = db.relationship('Login', backref=db.backref('text', lazy=True))
 
-    def __init__(self, text):
-        self.text = text.strip()
+
+    # def __init__(self, text):
+    #     self.text = text.strip()
 
     def __repr__(self):
         return '<Message %r>' % self.text
 
-
-class MessageNames():
-    def __init__(self, name, text):
-        self.name = [
-            Message(text=n.strip()) for n in name.split(',')
-        ]
-        self.text = [
-            Message(text=t.strip()) for t in text.split(',')
-        ]
-        #super().__init__(name, text)
+#
+# class MessageNames():
+#     # id = db.Column(db.Integer, primary_key=True)
+#     # name = db.Column(db.String(1024), nullable=False)
+#     # text = db.Column(db.String(32), nullable=False)
+#     def __init__(self, name, text):
+#         self.name = [
+#             Login(name=n.strip()) for n in name.split(',')
+#         ]
+#         self.text = [
+#             Message(text=t.strip()) for t in text.split(',')
+#         ]
+#         #super().__init__(name, text)
 
 
 db.create_all()
@@ -65,24 +76,48 @@ def login():
 def get_user():
     name = request.form['log__name']
     password = request.form['password']
+    user = Login(name=name, password=password)
 
-    db.session.add(Login(name, password))
+    db.session.add(user)
     db.session.commit()
 
-    return redirect(url_for('main'))
+    return redirect(url_for('main', user_id=user.id))
+
+# @app.route('/get_user', methods=['POST'])
+# def get_user_id():
+#     return add_message(Login.id)
 
 # messages = MessageNames()
-
+# db.session.query(Login, Message).filter(Login.id == Message.id).all()
 @app.route('/main', methods=['GET'])
 def main():
-    return render_template('main.html', login=Login.query.all(), messages=Message.query.all())
+    return render_template('main.html', result=db.session.query(Login.name, Message.text).filter(Login.id == Message.user_id).all())
 
 
-@app.route('/add_message', methods=['POST'])
+@app.route('/main/<id>', methods=['GET'])
+def get_name(id):
+    username = request.args.get('user_id')
+    # add_message(username)
+    return id
+
+
+@app.route('/add_message', methods=['GET', 'POST'])
 def add_message():
-    # name = request.form['nick']
+    # username = request.args.get('user_id')
+    # f = get_name()
+    # print(f)
+    # print("Madina!!!!!!!!!!!!!!!!!!")
+    name = Login.name
     # password = request.form['password']
     text = request.form['message']
+    # res=db.session.query(Login.name, Message.text).filter(Login.id == Message.user_id).all()
+    users = Login.query.get(37)
+    message = Message(text=text, user=users)
+    # message = 0
+    # for i in res[id]:
+    #     print(i)
+    #     message = Message(text=text, user=i)
+
 
 
     if len(text) == 0:
@@ -98,9 +133,15 @@ def add_message():
     #     )
     #     # return
 
-    db.session.add(Message(text))
+    db.session.add(message)
     db.session.commit()
 
     return redirect(url_for('main'))
 
+
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host = '127.0.0.1', port = 5000)
 
